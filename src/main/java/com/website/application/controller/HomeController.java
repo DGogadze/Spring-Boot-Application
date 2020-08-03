1410049ed870f7d2dd1638642d1f6a07edde3077
LordCommander21 <gogadzedaniel21@gmail.com>
2020-08-03 21:39:52 +0400

    connectedToDatabase

--- src/main/java/com/website/application/controller/HomeController.java
+++ src/main/java/com/website/application/controller/HomeController.java
@@ -1,11 +1,16 @@
 package com.website.application.controller;
 
+import com.website.application.model.User;
+import com.website.application.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
+    @Autowired
+    private UserRepository userRepository;
     @GetMapping("/")
     public String home(Model model){
         return "home";
@@ -25,5 +30,11 @@ public class HomeController {
     @GetMapping("/support")
     public String support(Model model){
         return "support";
+    }
+    @GetMapping("/getdata")
+    public String getData(Model model){
+        Iterable<User> users = userRepository.findAll();
+        model.addAttribute("users",users);
+        return "home";
     }
 }
---
+++ src/main/java/com/website/application/model/User.java
@@ -0,0 +1,51 @@
+package com.website.application.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long userID;
+
+    private String userName;
+
+    private String userPassword;
+
+    private String userEmail;
+
+    public void setUserID(Long userID) {
+        this.userID = userID;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public void setUserPassword(String userPassword) {
+        this.userPassword = userPassword;
+    }
+
+    public void setUserEmail(String userEmail) {
+        this.userEmail = userEmail;
+    }
+
+    public Long getUserID() {
+        return userID;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public String getUserPassword() {
+        return userPassword;
+    }
+
+    public String getUserEmail() {
+        return userEmail;
+    }
+}
