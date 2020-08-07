d211a8be70c2948bd8884759412c3ad6d565fa0e
LordCommander21 <gogadzedaniel21@gmail.com>
2020-08-08 01:17:26 +0400

    addedPasswordEncoding

--- src/main/java/com/website/application/controller/DatabaseController.java
+++ src/main/java/com/website/application/controller/DatabaseController.java
@@ -3,31 +3,32 @@ package com.website.application.controller;
 import com.website.application.model.User;
 import com.website.application.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 
 @Controller
 public class DatabaseController {
+    BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
     @Autowired
     UserRepository userRepository;
     @PostMapping("/signup")
     public String signUp(@RequestParam String userEmail,@RequestParam String userName,@RequestParam String userPassword, Model model){
         User user = new User(userEmail,userName,userPassword);
+        user.setUserPassword(bCryptPasswordEncoder.encode(userPassword));
         userRepository.save(user);
-        model.addAttribute("successSignUp","Account " + userName + " was successfully created!");
         return "signin";
     }
     @PostMapping("/signin")
     public String signIn(@RequestParam String userName,@RequestParam String userPassword,Model model){
         User user = userRepository.findByUserName(userName);
-        if (user.getUserPassword().equals(userPassword)){
-            model.addAttribute("userName",userName);
-            model.addAttribute("userID",user.getUserID());
+        if (bCryptPasswordEncoder.encode(userPassword).equals(user.getUserPassword())){
+            model.addAttribute("userName",user.getUserName());
             model.addAttribute("userEmail",user.getUserEmail());
+            model.addAttribute("userID",user.getUserID());
         }
         return "profile";
     }
