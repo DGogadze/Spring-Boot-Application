51a0a9b4d01296d268c6f0fcbe16377df1ac5954
LordCommander21 <gogadzedaniel21@gmail.com>
2020-08-13 23:04:50 +0400

    codeRefactory

--- src/main/java/com/website/application/security/MvcConfig.java
+++ src/main/java/com/website/application/security/MvcConfig.java
@@ -10,7 +10,5 @@ public class MvcConfig implements WebMvcConfigurer {
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/home").setViewName("home");
         registry.addViewController("/").setViewName("home");
-        registry.addViewController("/login").setViewName("signin");
-        registry.addViewController("/signup").setViewName("signup");
     }
 }
